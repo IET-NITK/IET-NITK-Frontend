b4d4cb9d2bd07f01e37e0b5740ec939c3af61a91
MBSAIADITYA <94775361+MBSAIADITYA@users.noreply.github.com>
2024-02-27 23:53:59 +0530

    Update page.js

--- src/app/blogs/[blogid]/page.js
+++ src/app/blogs/[blogid]/page.js
@@ -6,7 +6,7 @@ export default function BlogDetail({ params }){
   )
 }
 
-export async function generateStaticPaths() {
+export async function generateStaticParams() {
   const posts = await fetch('https://tetragram.codered.cloud/api/v2/pages/?type=blog.BlogPage&fields=*').then((res) => res.json())
    return posts.items.map((post) => ({
     blogid: post.blog_slug,
